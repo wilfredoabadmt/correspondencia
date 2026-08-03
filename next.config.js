1e359648bebd6c18c1b271f8023c86d5178491ae
Wilfredo Abad <wilfredoabad@gmail.com>
2026-08-02 21:47:33 -0400

    nuevo2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+    openAnalyzer: false, // Prevents opening the browser automatically
+});
+
+const nextConfig = withBundleAnalyzer({
     output: 'standalone',
-};
+});
 
 module.exports = nextConfig;
